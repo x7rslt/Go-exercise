48fc033a66f6eebdea664cee318acefc609bfe11
QIANYU <30388878+x7rslt@users.noreply.github.com>
2020-12-16 23:14:22 +0800

    os.UserHomeDir

--- tool_read_learn/ffuf_read/main_split/main.go
+++ tool_read_learn/ffuf_read/main_split/main.go
@@ -1,6 +1,13 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
 //main可以分拆成多个文件，运行时：go run main.go test.go 即可
-编译同样：go build main.go test.go
+//编译同样：go build main.go test.go
 func main() {
-	test()
+	//test()
+	fmt.Println(os.UserHomeDir())
 }
