ceec5476e08a7a512ef31b533e9c273f79e47246
QIANYU <30388878+x7rslt@users.noreply.github.com>
2020-12-27 22:24:17 +0800

    interface implement

--- tool_read_learn/ffuf_read/pkg/ffuf/job/input/main.go
+++ tool_read_learn/ffuf_read/pkg/ffuf/job/input/main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+)
+
+const (
+	SHELL_CMD = "cmd.exe"
+	SHELL_ARG = "/C"
+)
+
+type Request struct {
+	Method   string
+	Host     string
+	Url      string
+	Headers  map[string]string
+	Data     []byte
+	Input    map[string][]byte
+	Position int
+	Raw      string
+}
+
+// Response struct holds the meaningful data returned from request and is meant for passing to filters
+type Response struct {
+	StatusCode    int64
+	Headers       map[string][]string
+	Data          []byte
+	ContentLength int64
+	ContentWords  int64
+	ContentLines  int64
+	Cancelled     bool
+	Request       *Request
+	Raw           string
+	ResultFile    string
+}
+
 //RunnerProvider是一个请求执行者的接口
 type RunnerProvider interface {
 	Prepare(input map[string][]byte) (Request, error)
@@ -7,7 +46,7 @@ type RunnerProvider interface {
 }
 
 //InputProvider接口处理RunnerProvider的输入数据
-type InputProvider interface{
+type InputProvider interface {
 	AddProvider(InputProviderConfig) error
 	Next() bool
 	Position() int
@@ -17,7 +56,7 @@ type InputProvider interface{
 }
 
 //InternalInputProvider接口负责向InputProvider提供输入数据
-type InternalInputProvider interface{
+type InternalInputProvider interface {
 	Keyword() string
 	Next() bool
 	Position() int
@@ -30,35 +69,32 @@ type InputProviderConfig struct {
 	Value string
 }
 
-
 //作为InputProvider的实例
 type MainInputProvider struct {
-	Providers   [].InternalInputProvider
+	Providers   []InternalInputProvider
 	Config      *Config
 	position    int
 	msbIterator int
 }
-func NewInputProvider(conf *ffuf.Config) (ffuf.InputProvider, ffuf.Multierror) {
-	validmode := false
-	errs := ffuf.NewMultierror()
-	for _, mode := range []string{"clusterbomb", "pitchfork"} {
-		if conf.InputMode == mode {
-			validmode = true
-		}
-	}
-	if !validmode {
-		errs.Add(fmt.Errorf("Input mode (-mode) %s not recognized", conf.InputMode))
-		return &MainInputProvider{}, errs
-	}
+type Multierror struct {
+	errors []error
+}
+
+//NewMultierror returns a new Multierror
+func NewMultierror() Multierror {
+	return Multierror{}
+}
+
+func NewInputProvider(conf *Config) InputProvider {
 	mainip := MainInputProvider{Config: conf, msbIterator: 0}
-	// Initialize the correct inputprovider
-	for _, v := range conf.InputProviders {
-		err := mainip.AddProvider(v)
-		if err != nil {
-			errs.Add(err)
-		}
-	}
-	return &mainip, errs
+	return &mainip
+}
+
+func (i *MainInputProvider) AddProvider(provider InputProviderConfig) error {
+	return nil
+}
+func (i *MainInputProvider) Position() int {
+	return i.position
 }
 
 //Reset resets all the inputproviders and counters
@@ -70,27 +106,57 @@ func (i *MainInputProvider) Reset() {
 	i.msbIterator = 0
 }
 
-type Job struct{
+//Next will increment the cursor position, and return a boolean telling if there's inputs left
+func (i *MainInputProvider) Next() bool {
+	if i.position >= i.Total() {
+		return false
+	}
+	i.position++
+	return true
+}
+
+//Value returns a map of inputs for keywords
+func (i *MainInputProvider) Value() map[string][]byte {
+	retval := make(map[string][]byte)
+	retval["Value"] = []byte("No input value")
+	return retval
+}
+func (i *MainInputProvider) Total() int {
+	count := 0
+	if i.Config.InputMode == "pitchfork" {
+		for _, p := range i.Providers {
+			if p.Total() > count {
+				count = p.Total()
+			}
+		}
+	}
+	if i.Config.InputMode == "clusterbomb" {
+		count = 1
+		for _, p := range i.Providers {
+			count = count * p.Total()
+		}
+	}
+	return count
+}
+
+type Job struct {
 	Input InputProvider
 }
 
-
-
-
-func NewConfig(ctx context.Context,cancel context.CancelFunc)Config{
+func NewConfig(ctx context.Context, cancel context.CancelFunc) Config {
 	var conf Config
-	conf.InputProviders = make([]InputProviderConfig,0)
+	conf.InputProviders = make([]InputProviderConfig, 0)
 	return conf
 
 }
 
-
 type CommandInput struct {
 	config  *Config
 	count   int
+	command string
 }
 
-func NewCommandInput(conf *ffuf.Config) (*CommandInput, error) {
+func NewCommandInput(conf *Config) (*CommandInput, error) {
 	var cmd CommandInput
 	cmd.config = conf
 	cmd.count = 0
@@ -124,7 +190,14 @@ func (c *CommandInput) Next() bool {
 
 //Value returns the input from command stdoutput
 func (c *CommandInput) Value() []byte {
-	bb := []byte("Value for null")
+	var stdout bytes.Buffer
+	os.Setenv("FFUF_NUM", strconv.Itoa(c.count))
+	cmd := exec.Command(SHELL_CMD, SHELL_ARG, c.command)
+	cmd.Stdout = &stdout
+	err := cmd.Run()
+	if err != nil {
+		return []byte("")
+	}
 	return stdout.Bytes()
 }
 
@@ -132,18 +205,18 @@ func (c *CommandInput) Value() []byte {
 func (c *CommandInput) Total() int {
 	return c.config.InputNum
 }
+
 type Config struct {
-	InputNum int
+	InputNum       int
 	InputProviders []InputProviderConfig
+	InputMode      string
 }
 
-
-func main(){
+func main() {
 	ipc := InputProviderConfig{"ipc for null"}
-	conf := &Config{123, []InputProviderConfig{ipc}}
-	j := Job{
-
-	}
+	conf := &Config{123, []InputProviderConfig{ipc}, "InputMode null"}
+	newin := NewInputProvider(conf)
+	j := Job{newin}
 	j.Input.Reset()
 	fmt.Println(j)
 
