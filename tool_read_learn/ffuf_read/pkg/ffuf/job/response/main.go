736cad2cea0bd8c551cdd9a0702f2eb974185a4a
QIANYU <30388878+x7rslt@users.noreply.github.com>
2021-01-06 22:56:31 +0800

    ResolveReference

--- tool_read_learn/ffuf_read/pkg/ffuf/job/response/main.go
+++ tool_read_learn/ffuf_read/pkg/ffuf/job/response/main.go
@@ -1,44 +1,45 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 type Request struct {
-	Method string
-	Host string
-	Url string
-	Headers map[string]string
-	Data []byte
-	Input map[string][]byte
+	Method   string
+	Host     string
+	Url      string
+	Headers  map[string]string
+	Data     []byte
+	Input    map[string][]byte
 	Position int
-	Raw string
+	Raw      string
 }
 
-func NewRequest()Request{
+func NewRequest() Request {
 	var req Request
 	req.Method = "Get"
 	req.Url = "www.baidu.com"
-	req.Headers = Make(map[string]string)
+	req.Headers = make(map[string]string)
 	return req
 }
 
-type Response struct{
-	StatusCode int64
-	Headers map[string[]]string
-	Data []byte
+type Response struct {
+	StatusCode    int64
+	Data          []byte
 	Contentlength int64
-	ContentWords int64
-	ContentLines int64
-	Cancelled bool
-	Request *Request
-	Raw string
-	ResultFile string
+	ContentWords  int64
+	ContentLines  int64
+	Cancelled     bool
+	Request       *Request
+	Raw           string
+	ResultFile    string
 }
 
 func NewResponse(httpresp *http.Response, req *Request) Response {
 	var resp Response
 	resp.Request = req
 	resp.StatusCode = int64(httpresp.StatusCode)
-	resp.Headers = httpresp.Header
 	resp.Cancelled = false
 	resp.Raw = ""
 	resp.ResultFile = ""
@@ -46,9 +47,11 @@ func NewResponse(httpresp *http.Response, req *Request) Response {
 }
 
 func main() {
-	var httpreq *http.Request
-	data := bytes.NewReader([]byte("test"))
 	q := NewRequest()
+	urltest := "http://www.baidu.com"
+	r, nil := http.Get(urltest)
 	fmt.Println(q)
+	re := NewResponse(r, &q)
+	fmt.Println(nil, re, re.Request)
 
 }
--- tool_read_learn/ffuf_read/pkg/ffuf/job/url/main.go
+++ tool_read_learn/ffuf_read/pkg/ffuf/job/url/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/url"
 )
 
 func main() {
-	urltest := "http://www.baidu.com"
+	urltest := "http://www.baidu.com/test"
 	redirectUrl, err := url.Parse(urltest)
 	fmt.Println(redirectUrl, err)
 	v := url.Values{}
@@ -18,4 +19,14 @@ func main() {
 	fmt.Println(v.Get("name"))
 	fmt.Println(v.Get("friend"))
 	fmt.Println(v["friend"], v.Encode())
+	u, err := url.Parse("../..//search?q=urltest")
+	if err != nil {
+		log.Fatal(err)
+	}
+	base, err := url.Parse(urltest)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(base.ResolveReference(u))
+
 }
