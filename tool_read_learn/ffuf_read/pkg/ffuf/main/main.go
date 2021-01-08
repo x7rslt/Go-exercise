724a5bff4ed658e8574598cfdb15ffc34e424ce0
QIANYU <30388878+x7rslt@users.noreply.github.com>
2021-01-08 22:22:38 +0800

    Set test

--- tool_read_learn/ffuf_read/pkg/ffuf/main/main.go
+++ tool_read_learn/ffuf_read/pkg/ffuf/main/main.go
@@ -7,9 +7,16 @@ type multiStringFlag []string
 func (m *multiStringFlag) String() string {
 	return "lehehe"
 }
+func (m *multiStringFlag) Set(value string) error {
+	*m = append(*m, value)
+	return nil
+}
 
 func main() {
 	fmt.Print("test")
 	var t multiStringFlag
+	fmt.Print(t)
 	fmt.Print(t.String())
+	t.Set("hello")
+	fmt.Println(t)
 }
