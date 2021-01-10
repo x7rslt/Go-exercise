e3ecfc44c45439d61374c0c8d1864543a417522a
QIANYU <30388878+x7rslt@users.noreply.github.com>
2021-01-10 22:34:40 +0800

    flag parse test

---
+++ tool_read_learn/ffuf_read/pkg/ffuf/main/flag/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var nFlag = flag.Int("n", 1234, "help message for flag n")
+var flagvar int
+
+func init() {
+	flag.IntVar(&flagvar, "flagname", 1234, "help message for flagname")
+}
+func main() {
+
+	flag.Parse()
+	fmt.Println(*nFlag)
+	fmt.Println(flagvar)
+
+}
+flag 待学习
