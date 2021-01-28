174c2693aa0dc9d7d26a7843227ed797dfe07324
x7rslt <1660019216@qq.com>
2021-01-28 23:03:17 +0800

    ffuf rebuild

---
+++ tool_read_learn/ffuf_read/select/chan/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+//main.go test wordlist Set()
+func main() {
+	fmt.Println("tset")
+	ch := make(chan int, 1)
+	go func() {
+		ch <- 10
+		//fmt.Println(<-ch) 在这里没法接收，要在goroutine外面接收
+	}()
+	fmt.Println(<-ch)
+
+}
---
+++ tool_read_learn/ffuf_read/select/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	ch := make(chan int, 1)
+	go func() {
+		for i := 0; i < 10; i++ {
+			select {
+			case <-ch:
+				//"<-ch"在这里只做情景判断，不算接收
+				//fmt.Println(<-ch) 算接受，循环结束
+				fmt.Println("To conitue")
+			case ch <- i: //第一步：i传入ch
+				fmt.Println("ch", i)
+				fmt.Println(<-ch)
+			}
+		}
+	}()
+	fmt.Println("test") //如果没有这里的接收，程序无输出
+
+	//没搞懂为什么和simple_test的情况相同
+}
