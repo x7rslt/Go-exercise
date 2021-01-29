9a529a393780284dd72c9fd5730334433b9f023b
x7rslt <1660019216@qq.com>
2021-01-29 21:46:25 +0800

    switch test

--- tool_read_learn/ffuf_read/select/main.go
+++ tool_read_learn/ffuf_read/select/main.go
@@ -16,11 +16,11 @@ func main() {
 				fmt.Println("To conitue")
 			case ch <- i: //第一步：i传入ch
 				fmt.Println("ch", i)
-				fmt.Println(<-ch)
+
 			}
 		}
 	}()
-	fmt.Println("test") //如果没有这里的接收，程序无输出
-
+	fmt.Println("test")
+	fmt.Println(<-ch) //如果没有这里的接收，程序无输出
 	//没搞懂为什么和simple_test的情况相同
 }
