fb8e1a165697e47032233a98a0994d7354de3272
x7rslt <1660019216@qq.com>
2021-01-31 15:53:04 +0800

    Select test

--- tool_read_learn/ffuf_read/select/select_test.go
+++ tool_read_learn/ffuf_read/select/select_test.go
@@ -3,6 +3,7 @@ package select_test
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 //The select statement lets a goroutine wait on multipe communciation
@@ -34,4 +35,51 @@ func Test(t *testing.T) {
 	}()
 	fibonacci(c, quit)
 
+}
+
+//Select default:
+//The default case in a select is run no other case is ready.
+// Use a default case to try a send or receive without blocking.
+
+func TestSelectDefault(t *testing.T) {
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return //通道使用return终端后续
+		default:
+			fmt.Println("   .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
+
+//Select exercise
+func PT(a, b chan string) {
+	text := "Sunshine."
+	for {
+		select {
+		case a <- text:
+			fmt.Println("Get something.")
+		case end := <-b:
+			fmt.Println(end, "Quit.")
+			return
+		}
+	}
+}
+
+func TestS(t *testing.T) {
+	a := make(chan string)
+	b := make(chan string)
+	go func() {
+		for i := 0; i < 3; i++ {
+			fmt.Println(<-a)
+		}
+		b <- "Quit time."
+	}()
+	PT(a, b)
 }
