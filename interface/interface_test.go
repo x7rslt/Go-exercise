2d61c2ecc768f647ee406889288cc74d3a96e645
x7rslt <1660019216@qq.com>
2021-03-22 20:48:11 +0800

    interface type switch

--- interface/interface_test.go
+++ interface/interface_test.go
@@ -22,3 +22,41 @@ func TestInterface(t *testing.T) {
 	var xiao Man = Man{"Femal", 27}
 	xiao.Resume()
 }
+
+//interface可以接受任意值
+var emptyInterface interface{}
+
+func Test(t *testing.T) {
+	emptyInterface = 10
+	fmt.Printf("%T\n", emptyInterface)
+
+	emptyInterface = 10.0
+	fmt.Printf("%T\n", emptyInterface)
+
+	emptyInterface = 10.00
+	fmt.Printf("%T\n", emptyInterface)
+
+	emptyInterface = "Hello world"
+	fmt.Printf("%T\n", emptyInterface)
+
+}
+
+func printType(i interface{}) {
+	switch val := i.(type) {
+	case int:
+		fmt.Println("int")
+	case string:
+		fmt.Println("string")
+	case float64:
+		fmt.Println("float64")
+	default:
+		fmt.Println("Other:", val)
+	}
+}
+
+func TestType(t *testing.T) {
+	var i interface{}
+	printType(i)
+	printType(10)
+	printType("hello")
+}
