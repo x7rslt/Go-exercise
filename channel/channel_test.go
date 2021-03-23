6c5f1317af9b62086d8252f3a85e4ccd6eb3dd3a
x7rslt <1660019216@qq.com>
2021-03-23 22:12:26 +0800

    channel

---
+++ channel/channel_test.go
@@ -0,0 +1,53 @@
+package channel_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestChannel(t *testing.T) {
+	ch := make(chan int)
+	done := make(chan struct{})
+	i1 := 10
+	go func() {
+		ch <- i1
+		done <- struct{}{}
+	}()
+	fmt.Println(<-ch)
+	<-done
+	fmt.Println("Channel Done.")
+}
+
+func TestChannelN(t *testing.T) {
+	ch := make(chan int, 10)
+	done := make(chan struct{})
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+	}()
+
+	go func() {
+		for v := range ch {
+			fmt.Println(v)
+		}
+		done <- struct{}{}
+	}()
+	<-done
+	fmt.Println("Main finished.")
+}
+
+func TestChannelWR(t *testing.T) {
+	ch := make(chan int, 2)
+	for i := 0; i < 10; i++ {
+		select {
+		case ch <- i:
+			fmt.Println("Sent to channel", i)
+		case i2 := <-ch:
+			fmt.Println("Receieved from channel", i2)
+		default:
+			fmt.Println("None action.")
+		}
+	}
+}
