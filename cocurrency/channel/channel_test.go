de15a1317dea8023ce2eeb20ecc5c4b5277f1557
x7rslt <1660019216@qq.com>
2021-04-10 23:25:40 +0800

    channel question

--- cocurrency/channel/channel_test.go
+++ cocurrency/channel/channel_test.go
@@ -2,6 +2,7 @@ package channel_test
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 	"time"
 )
@@ -39,4 +40,52 @@ func TestChannelTimeoutFunc(t *testing.T) {
 	case <-timeout:
 		fmt.Println("Time out.")
 	}
+}
+
+//问题：接收的数据只有1、3、5、7、9？为什么？
+func TestChannel(t *testing.T) {
+	ch := make(chan int, 10)
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+		go func(v int) {
+			//fmt.Println(i)
+			ch <- v
+		}(i)
+	}
+
+	go func() {
+		for range ch {
+			fmt.Println(<-ch)
+		}
+	}()
+
+	runtime.Gosched()
+	fmt.Println("Done")
+}
+
+//问题：输出莫名其妙的数字？
+func TestChannel2(t *testing.T) {
+	ch := make(chan int, 10)
+	for i := 0; i < 10; i++ {
+		//fmt.Println(i)
+		go func(v int) {
+			//fmt.Println(i)
+			ch <- v
+		}(i)
+	}
+
+	for range ch {
+		timer()
+		select {
+		case <-ch:
+			fmt.Println(<-ch)
+		case <-timeout:
+			fmt.Println("Timeout.")
+			fmt.Println("Done")
+		}
+
+	}
+
+	runtime.Gosched()
+	fmt.Println("Done")
 }
