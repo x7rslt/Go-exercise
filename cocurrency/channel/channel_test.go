b52dda368cd6218dece3a439b1ac2a07dc73d9e8
x7rslt <1660019216@qq.com>
2021-04-10 22:53:46 +0800

    channel timeout func

--- cocurrency/channel/channel_test.go
+++ cocurrency/channel/channel_test.go
@@ -20,3 +20,23 @@ func TestChannelTimeout(t *testing.T) {
 		fmt.Println("Time out.")
 	}
 }
+
+var timeout chan bool
+
+func timer() {
+	timeout = make(chan bool, 1)
+	go func() {
+		time.Sleep(1e9)
+		timeout <- true
+	}()
+}
+func TestChannelTimeout2(t *testing.T) {
+	ch := make(chan int)
+
+	timer()
+	select {
+	case <-ch:
+	case <-timeout:
+		fmt.Println("Time out.")
+	}
+}
