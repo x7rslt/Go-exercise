4ade239db88e1b3fd2b3f01a70e86a073c98e2fb
x7rslt <1660019216@qq.com>
2021-04-11 22:41:32 +0800

    channel cpu2

--- cocurrency/channel/channel_cpu/channelcpu_test.go
+++ cocurrency/channel/channel_cpu/channelcpu_test.go
@@ -10,3 +10,14 @@ func TestChannelCpu(t *testing.T) {
 	num_cpu := runtime.NumCPU()
 	fmt.Println(runtime.GOMAXPROCS(num_cpu))
 }
+
+func TestCpuThread(t *testing.T) {
+	runtime.GOMAXPROCS(2)
+	for {
+		go func() {
+			fmt.Println(0)
+		}()
+		fmt.Println(1)
+	}
+
+}
