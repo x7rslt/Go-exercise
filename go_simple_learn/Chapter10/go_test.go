50ff6546b12fff0315ccdcca83850a199726dea0
x7rslt <1660019216@qq.com>
2021-04-17 11:31:33 +0800

    sync lock unlock

--- go_simple_learn/Chapter10/go_test.go
+++ go_simple_learn/Chapter10/go_test.go
@@ -2,6 +2,8 @@ package test
 
 import (
 	"fmt"
+	"runtime"
+	"sync"
 	"testing"
 )
 
@@ -34,4 +36,44 @@ func TestCloseChannel(t *testing.T) {
 	for i := range c {
 		fmt.Println(i)
 	}
+}
+
+//竞态
+var sum int
+
+func Sum(i int) {
+	sum += i
+}
+func GetSum() int {
+	return sum
+}
+func TestCompete(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		go Sum(i) //不同状态的goroutine引用同一个数据，容易重复引用，导致结果错误
+	}
+	fmt.Println(GetSum())
+}
+
+//避免竞态
+var sum2 int
+var m sync.Mutex
+
+func Sum2(i int) {
+	m.Lock()
+	defer m.Unlock()
+	sum2 += i
+}
+
+func GetSum2() int {
+	m.Lock()
+	defer m.Unlock()
+	return sum2
+}
+
+func TestCompeteSolve(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		go Sum2(i)
+	}
+	runtime.Gosched()
+	fmt.Println(GetSum2())
 }
