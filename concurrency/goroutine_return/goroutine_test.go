f8e4d3e16691b536c3208ca58c13f7f4d7ed8a2c
x7rslt <1660019216@qq.com>
2021-05-17 21:18:39 +0800

    cocurrency -> concurrency

---
+++ concurrency/goroutine_return/goroutine_test.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Result struct {
+	name   string
+	length int
+}
+
+func PersonCount(userlist []string, ch chan Result) {
+	defer close(ch)
+	result := new(Result)
+	for _, i := range userlist {
+		result.name = i
+		result.length = len(i)
+		ch <- *result
+	}
+
+}
+
+func TestChannelReturn(t *testing.T) {
+	userlist := []string{
+		"lehehe", "muhahah", "kaixin",
+	}
+	ch := make(chan Result)
+	go PersonCount(userlist, ch)
+	resultlist := []Result{}
+	for i := range ch {
+		resultlist = append(resultlist, i)
+	}
+	fmt.Println(resultlist)
+}
