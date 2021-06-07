2d74f1cd355c37487d988d044d96cfa6fe654e25
x7rslt <1660019216@qq.com>
2021-06-07 23:00:58 +0800

    repository model discount

---
+++ book_final_code/Chapter16/repository/discount.go
@@ -0,0 +1,11 @@
+package repository
+
+type Discount struct {
+	DB model.DataBase
+}
+
+func (d *Discount) ListDiscounts(pos int) []model.Discount {
+	var discounts []model.Discount
+	d.DB.MyDB.Where("pos=?", pos).Find(&discounts)
+	return discounts
+}
