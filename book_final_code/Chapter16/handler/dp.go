ef1a20f3ca7e01a7160e70b03e87879ec459dd89
QIANYU <30388878+x7rslt@users.noreply.github.com>
2021-06-14 22:36:51 +0800

    handler hoteldetailhandler

---
+++ book_final_code/Chapter16/handler/dp.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"book_final_code/Chapter16/service/dp_service"
+	"food/dp_service"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+const domain = "http://192.168.0.104:8080"
+
+type HotelDetailHandler struct {
+	Srv *dp_service.HotelService
+}
+
+func (h *HotelDetailHandler) HotelDetailHandler(c *gin.Context) {
+	hotelId := c.Param("id")
+
+	hotel, err := h.Srv.GetHotelDetailByID(hotelId)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"item": nil,
+			"msg":  err.Error(),
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"item": hotel,
+			"msg":  "",
+		})
+	}
+}
