d166e4770a81c45e8f60b815d572c64f64453532
QIANYU <30388878+x7rslt@users.noreply.github.com>
2021-07-09 21:01:15 +0800

    food res

---
+++ book_final_code/Chapter16/res/hotelFoodCategoryResp.go
@@ -0,0 +1,7 @@
+package res
+
+type HotelFoodCategoryResp struct {
+	HotelFoodCategoryId string `json:"hotelFoodCategoryId"`
+	HotelFoodCategoryName string `json:"hotelFoodCategoryName"`
+	HotelId string `json:"hotelId"`
+}
