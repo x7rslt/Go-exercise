4459cab91ec6a0e0864bf02a48219966cca2a5f4
QIANYU <30388878+x7rslt@users.noreply.github.com>
2021-07-16 19:48:19 +0800

    respentity modify

--- book_final_code/Chapter16/res/RespEntity.go
+++ book_final_code/Chapter16/res/RespEntity.go
@@ -1,36 +1,15 @@
 package res
 
 import (
-<<<<<<< HEAD
 	"food/myerr"
 	"github.com/gin-gonic/gin"
-=======
-	"github.com/gin-gonic/gin"
-	"food/myerr"
->>>>>>> 3bc95edaf0eee6696f3b7d4b974b987237244d4f
 	"net/http"
 )
 
 type Response struct {
-<<<<<<< HEAD
 	Code    int         `json:"code"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
-}
-
-func SendResponse(c *gin.Context, err error, data interface{}) {
-	code, message := myerr.DecodeErr(err)
-
-	//http.StatusOK这个值是200
-	c.JSON(http.StatusOK, Response{
-		Code:    code,
-		Message: message,
-		Data:    data,
-=======
-	Code int `json:"code"`
-	Message string `json:"message"`
-	Data interface{} `json:"data"`
-
 }
 func SendResponse(c *gin.Context,err error,data interface{}){
 	code,message := myerr.DecodeErr(err)
@@ -38,6 +17,5 @@ func SendResponse(c *gin.Context,err error,data interface{}){
 		Code:code,
 		Message: message,
 		Data:data,
->>>>>>> 3bc95edaf0eee6696f3b7d4b974b987237244d4f
 	})
 }
