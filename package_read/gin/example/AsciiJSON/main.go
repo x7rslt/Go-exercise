4f19940d847e4af9f5aeb1d320a1713483d5d45e
x7rslt <1660019216@qq.com>
2021-07-19 21:43:27 +0800

    gin example

---
+++ package_read/gin/example/AsciiJSON/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main(){
+	r := gin.Default()
+	r.GET("/someJSON",func(c *gin.Context){
+		data := map[string]interface{}{
+			"lang":"GO language",
+			"tag":"<br>",
+		}
+
+		c.AsciiJSON(http.StatusOK,data)
+
+	})
+	r.Run()
+}
