9bf2596d0076f6b9676149da41adf0c8326d315d
x7rslt <1660019216@qq.com>
2021-08-19 23:35:16 +0800

    泛型

---
+++ package_read/go117/01/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func printSlice[T any](s []T){
+	for _,v := range s{
+		fmt.Printf("%v",v)
+	}
+	fmt.Print("\n")
+}
+
+func main(){
+	printSlice[int]([]int{66,77,88,99,100})
+	printSlice[float64]([]float64{1.1,2.2,3.3,4.4,5.5})
+	printSlice[string]([]string{"红烧肉","清蒸鱼","大闸蟹","九转大肠","重烧海参"})
+	printSlice([]int64{55,66,77,88,100})
+}
