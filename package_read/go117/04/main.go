eb66a44c9e69ba8433e948111bfcfa52dd10a7ae
x7rslt <1660019216@qq.com>
2021-08-21 23:02:11 +0800

    go117 泛型

---
+++ package_read/go117/04/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Price int
+
+type ShowPrice interface{
+	String() string
+}
+
+func(p Price)String()string{
+	return strconv.Itoa(int(p))
+}
+
+
+func showPriceList[T ShowPrice](s []T)(ret []string){
+	for idx,v := range s{
+		ret = append(ret,"第" + strconv.Itoa(idx+1)+"道菜价格是："+v.String())
+	}
+	return ret
+}
+
+func main(){
+	fmt.Println(showPriceList([]Price{12,34,55,66,77,88}))
+}
