2cedbb9c910216e9f094f6bf845ebf0713fce6e4
x7rslt <1660019216@qq.com>
2021-09-19 22:41:26 +0800

    Time

--- time/time_test.go
+++ time/time_test.go
@@ -35,3 +35,57 @@ func duration(msg string, start time.Time) {
 func TestFuncTime(t *testing.T){
 	foo()
 }
+
+//Format a time or date
+
+const (
+	layoutISO = "2006-01-02"
+	layoutUS  = "January 2, 2006"
+)
+func TestBasicTimeFormat(t *testing.T){
+	date := "1999-12-31"
+	time1, _ := time.Parse(layoutISO, date)
+	fmt.Println(time1)                  // 1999-12-31 00:00:00 +0000 UTC
+	fmt.Println(time1.Format(layoutUS)) // December 31, 1999
+}
+
+
+// TimeIn returns the time in UTC if the name is "" or "UTC".
+// It returns the local time if the name is "Local".
+// Otherwise, the name is taken to be a location name in
+// the IANA Time Zone database, such as "Africa/Lagos".
+func TimeIn(t time.Time, name string) (time.Time, error) {
+	loc, err := time.LoadLocation(name)
+	if err == nil {
+		t = t.In(loc)
+	}
+	return t, err
+}
+
+func TestLocal(t *testing.T){
+	for _, name := range []string{
+		"",
+		"Local",
+		"Asia/Shanghai",
+		"America/Metropolis",
+	} {
+		t, err := TimeIn(time.Now(), name)
+		if err == nil {
+			fmt.Println(t.Location(), t.Format("15:04"))
+		} else {
+			fmt.Println(name, "<time unknown>")
+		}
+	}
+}
+
+//get current timestamp
+
+func TestCurrentTime(t *testing.T){
+	now := time.Now()      // current local time
+	sec := now.Unix()      // number of seconds since January 1, 1970 UTC
+	nsec := now.UnixNano() // number of nanoseconds since January 1, 1970 UTC
+
+	fmt.Println(now)  // time.Time
+	fmt.Println(sec)  // int64
+	fmt.Println(nsec) // int64
+}
