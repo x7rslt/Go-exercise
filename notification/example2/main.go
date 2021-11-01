9ffff433503737628570901e26bd9c3094c57330
QIANYU <30388878+x7rslt@users.noreply.github.com>
2021-11-01 17:05:01 +0800

    beep notification

---
+++ notification/example2/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	f, err := os.Open("bgm.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer streamer.Close()
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
+
+	<-done
+}
